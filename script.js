27666d768b517561a1ecff1c30e13ae084f7301a
Geghard-S <geghard@gmail.com>
2021-05-21 00:08:37 -0700

    third commit

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const hourEL = document.querySelector('.hour')
-const minuteEL = document.querySelector('.minute')
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
-const dateEL = document.querySelector('.date')
+const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
-    if(html.classList.contains('dark')) {
+    if (html.classList.contains('dark')) {
         html.classList.remove('dark')
-        e.target.innerHTML = 'Dark Mode'
+        e.target.innerHTML = 'Dark mode'
     } else {
         html.classList.add('dark')
-        e.target.innerHTML = 'Light Mode'
+        e.target.innerHTML = 'Light mode'
     }
 })
+
 
 function setTime() {
     const time = new Date();
